808e857c20e7ead264ba1ae5ac1510b31cdfeb58
PlayLand Test <test@playland.ru>
2025-06-06 23:16:52 +0300

    Clean Paper test - removed all PlayLand modifications

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    java
+    `maven-publish`
+    id("io.papermc.paperweight.patcher") version "1.7.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        content { onlyForConfigurations("paperclip") }
+    }
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
+    decompiler("org.vineflower:vineflower:1.10.1")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release = 21
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = "UTF-8"
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = "UTF-8"
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+paperweight {
+    serverProject = project(":playland-server")
+
+    remapRepo = "https://maven.quiltmc.org/repository/release/"
+    decompileRepo = "https://maven.quiltmc.org/repository/release/"
+
+    useStandardUpstream("paper") {
+        url = github("PaperMC", "Paper")
+        ref = providers.gradleProperty("paperRef")
+
+        withStandardPatcher {
+            baseName("Paper")
+
+            apiPatchDir = layout.projectDirectory.dir("patches/api")
+            apiOutputDir = layout.projectDirectory.dir("playland-api")
+
+            serverPatchDir = layout.projectDirectory.dir("patches/server")
+            serverOutputDir = layout.projectDirectory.dir("playland-server")
+        }
+    }
+}
+
+tasks.register("printMinecraftVersion") {
+    doLast {
+        println(providers.gradleProperty("mcVersion").get())
+    }
+}
+
+tasks.register("printPaperVersion") {
+    doLast {
+        println(providers.gradleProperty("paperRef").get())
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "playland"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include("playland-api", "playland-server")
